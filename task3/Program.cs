77581192ec2125a7a720f9dc949ab00914d54065
Artem Zabrodenko <temazasport@mail.ru>
2022-11-22 19:41:31 +0800

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте массив. Напишите программу, которая
+//определяет, присутствует ли заданное число в массиве.
+
+int[] arr = InitialArr(10);
+
+
+
+System.Console.WriteLine("input number");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (CheckDigit(arr, num))
+{
+    System.Console.WriteLine("yes");
+}
+else
+{
+    System.Console.WriteLine("no");
+}
+// CheckDigit(arr, num);
+
+PrintArray(arr);
+
+
+int[] InitialArr(int count)
+{
+    Random rand = new Random();
+    int [] arr = new int[count];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(-9,10);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+}
+
+bool CheckDigit(int[] arr, int value)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i]==Math.Abs(value))
+        {
+            
+            return true;
+        }
+        
+    }
+    return false;
+}
