d053ac4dc02372f99694ce58ee2ad9c6abbbf091
Artem <temazasport@mail.ru>
2022-11-22 16:46:05 +0800

    task_2

---
+++ task2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+
+int count = 10;
+ 
+int[] arr = InitialArr(count);
+
+PrintArray(arr);
+
+int[] ReversArr = ReverseArr(arr);
+
+System.Console.WriteLine();
+
+PrintArray(ReversArr);
+
+
+int[] ReverseArr(int[] arr)
+{
+    int[] reversarr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        reversarr[i] = arr[i]*(-1);
+    }
+    return reversarr;
+}
+
+int[] InitialArr(int count)
+{
+    Random rand = new Random();
+    int [] arr = new int[count];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(-9,10);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+}
