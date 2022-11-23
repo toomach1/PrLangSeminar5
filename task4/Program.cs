5410a863c66ff4a0e04e754688e1dc6ae3b4b417
Artem <temazasport@mail.ru>
2022-11-23 10:02:11 +0800

    task_4

---
+++ task4/Program.cs
@@ -0,0 +1,42 @@
+﻿//  Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+
+int[] arr = GetRandomArray(5, -150, 150);
+
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i]>= 10 && arr[i]<=99) 
+    {
+        count ++;
+    }
+}
+
+PrintArray(arr);
+
+// System.Console.WriteLine($"колличество элеиентов массива в отрезке [10,99] = {count});
+
+System.Console.WriteLine("колличество элеиентов массива в отрезке [10,99] = " + count);
+
+int[] GetRandomArray(int size, int begin, int last)
+{
+    int[] arr = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = random.Next(begin, last + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+}
